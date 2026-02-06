4ffa4b1441411556e2141fbebc0438eb9544c6b9
Christian Uhsat <christian@uhsat.de>
2026-02-06 21:14:52 +0100

    initial release

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# bootkey
-Extract the BootKey from a Windows System Hive
+# BootKey
+Extract the BootKey from an offline system hive.
+
+[![Go Reference](https://pkg.go.dev/badge/github.com/cuhsat/bootkey.svg)](https://pkg.go.dev/github.com/cuhsat/bootkey)
+[![Go Report Card](https://goreportcard.com/badge/github.com/cuhsat/bootkey?style=flat-square)](https://goreportcard.com/report/github.com/cuhsat/bootkey)
+[![Release](https://img.shields.io/github/release/cuhsat/bootkey.svg?style=flat-square)](https://github.com/cuhsat/bootkey/releases/latest)
+
+
+```console
+go install github.com/cuhsat/bootkey@latest
+```
+
+## Usage
+```console
+$ bootkey SYSTEM
+```
+
+## License
+Released under the [MIT License](LICENSE.md).
---
+++ main.go
@@ -0,0 +1,107 @@
+// Extract the BootKey from an offline system hive.
+//
+// Usage:
+//
+//	bootkey system
+//
+// The arguments are:
+//
+//	system
+//	    The system registry hive (required).
+package main
+
+import (
+	"bytes"
+	"encoding/hex"
+	"fmt"
+	"os"
+
+	"golang.org/x/text/encoding/unicode"
+	"www.velocidex.com/golang/regparser"
+)
+
+const offset = 4096 + 4
+
+var keys = []string{"JD", "Skew1", "GBG", "Data"}
+var sbox = []int{8, 5, 4, 2, 11, 9, 13, 3, 0, 6, 1, 12, 14, 10, 15, 7}
+
+func extract(path string) (key []byte, err error) {
+	f, err := os.Open(path)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer func() { _ = f.Close() }()
+
+	reg, err := regparser.NewRegistry(f)
+
+	if err != nil {
+		return nil, err
+	}
+
+	cs := "ControlSet001"
+
+	// get current control set
+	if k := reg.OpenKey("\\Select"); k != nil {
+		for _, v := range k.Values() {
+			if v.ValueName() == "Current" {
+				cs = fmt.Sprintf("ControlSet%03d", v.ValueData().Uint64)
+			}
+		}
+	}
+
+	buf := bytes.NewBuffer(nil)
+
+	// extract key parts hidden in key class names
+	for _, v := range keys {
+		k := reg.OpenKey(fmt.Sprintf("\\%s\\Control\\Lsa\\%s", cs, v))
+		b := make([]byte, k.ClassLength())
+
+		_, err = reg.BaseBlock.HiveBin().Reader.ReadAt(b, int64(k.Class()+offset))
+
+		if err != nil {
+			return nil, err
+		}
+
+		buf.Write(b)
+	}
+
+	tmp := buf.String()
+
+	// decode unicode step
+	if buf.Len() > 32 {
+		dec := unicode.UTF16(unicode.LittleEndian, unicode.IgnoreBOM).NewDecoder()
+		tmp, _ = dec.String(buf.String())
+	}
+
+	// decode hex step
+	sub, err := hex.DecodeString(tmp)
+
+	if err != nil {
+		return nil, err
+	}
+
+	// transform key bytes
+	for i := 0; i < len(sub); i++ {
+		key = append(key, sub[sbox[i]])
+	}
+
+	return key, nil
+}
+
+func main() {
+	if len(os.Args) == 1 || os.Args[1] == "--help" {
+		_, _ = fmt.Fprintln(os.Stderr, "usage: bootkey SYSTEM")
+		os.Exit(2)
+	}
+
+	key, err := extract(os.Args[1])
+
+	if err != nil {
+		_, _ = fmt.Fprintln(os.Stderr, err)
+		return
+	}
+
+	fmt.Printf("%x\n", key)
+}
